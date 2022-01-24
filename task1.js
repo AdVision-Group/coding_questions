b94c4ac7174eb129dbfb59963d5507ad8fc8f2c1
flagg2 <samo.wittlinger@gmail.com>
2022-01-24 14:34:57 +0100

    questions with answers

---
+++ task1.js
@@ -0,0 +1,28 @@
+// Write a function that removes duplicates from an array
+
+function removeDuplicates(array) {
+   return [new Array(...new Set(array))];
+}
+
+function areArraysEqual(array1, array2) {
+   array1.sort();
+   array2.sort();
+   return array1.length === array2.length && array1.every((element, index) => element === array2[index]);
+}
+
+function runTests() {
+   console.assert(
+      areArraysEqual(removeDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+      "Test 1 failed"
+   );
+   console.assert(
+      areArraysEqual(
+         removeDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      ),
+      "Test 2 failed"
+   );
+   console.assert(areArraysEqual(removeDuplicates([(1, 1, 1, 1, 1, 1, 2)]), [1, 2]), "Test 3 failed");
+}
+
+runTests();
---
+++ task2.js
@@ -0,0 +1,22 @@
+// Write a function that checks whether a number is prime
+
+function isPrime(n) {
+   if (n < 2) return false;
+   for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+         return false;
+      }
+   }
+   return true;
+}
+
+console.assert(!isPrime(1), "Number 1 is not prime");
+console.assert(isPrime(2), "Number 2 is prime");
+console.assert(isPrime(3), "Number 3 is prime");
+console.assert(!isPrime(4), "Number 4 is not prime");
+console.assert(isPrime(5), "Number 5 is prime");
+console.assert(isPrime(7), "Number 7 is prime");
+console.assert(isPrime(11), "Number 11 is prime");
+console.assert(!isPrime(12), "Number 12 is not prime");
+console.assert(isPrime(1321), "Number 1321 is prime");
+console.assert(!isPrime(832911), "Number 832911 is not prime");
